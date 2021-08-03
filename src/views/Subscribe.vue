21fef6615f4b45fd9f372f5f02de9c03dabeec93
Ranjani <ranjanikarthi01@gmail.com>
2021-08-03 15:12:48 -0700

    about,subscribe pg modified

--- src/views/Subscribe.vue
+++ src/views/Subscribe.vue
@@ -7,12 +7,12 @@
         <div class="box">
         <div class="tbl">
             <center>
-                <table cellpadding="10" cellspacing="10">
+                <table>
                     <tr class=" border-bottom b1">
                         <th></th>
-                        <th><button class="bg-success">FREE</button></th>
-                        <th><button class="bg-warning ms-3">VIP</button></th>
-                        <th><button class="bg-info">PREMIUM</button></th>
+                        <th><button class="bg-success rounded bt">FREE</button></th>
+                        <th><button class="bg-warning ms-4  rounded  bt">VIP</button></th>
+                        <th><button class="bg-info  rounded ms-3 bt">PREMIUM</button></th>
                    </tr>
                    <tr class="border-bottom ">
                        
@@ -74,23 +74,23 @@
         </div>
         <div class="content">
             <div class="txt1">
-                <span><button class="bg-success fs-15px">VIP</button></span>&nbsp;&nbsp;
+                <span><button class="bg-success  rounded">VIP</button></span>&nbsp;&nbsp;
                 <br>
                 <span>₹399/Year</span>
             </div>&nbsp;
             <div class="txt1">
-                <span><button  class="bg-warning fs-15px">PREMIUM</button></span>&nbsp;&nbsp;
+                <span><button  class="bg-warning  rounded">PREMIUM</button></span>&nbsp;&nbsp;
                <br>
                 <span>₹1499/Year</span>
             </div>&nbsp;
             <div class="txt1">
-                <span><button  class="bg-info fs-15px">PREMIUM</button></span>&nbsp;&nbsp;
+                <span><button  class="bg-info  rounded">PREMIUM</button></span>&nbsp;&nbsp;
                 <br>
                 <span>₹299/Month</span>
             </div>
         </div><br>
         <div class="content2">
-          <h3 align="center">CONTINUE</h3>
+          <h4 align="center">CONTINUE</h4>
         </div>
         </div>
         </center>
@@ -130,7 +130,9 @@ body {
 .c2 {
   color: blue;
 }
-
+.bt{
+  font-size:13px;
+}
 .size {
   font-size: 15px;
   color: white;
@@ -145,10 +147,10 @@ body {
   border: 2px solid rgb(77, 77, 99);
   border-radius: 10px;
   width: 220px;
-  height: 70px;
+  height: 60px;
   display: inline-block;
-  font-size: 20px;
-  padding-top: 5px;
+  font-size: 16px;
+  padding-top: 3px;
 }
 .txt1:hover {
   cursor: pointer;
@@ -158,10 +160,22 @@ body {
   border: 1px solid #27385a;
   background: rgb(38, 82, 226);
   height: 40px;
-  padding-top: 7px;
+  padding-top: 5px;
   border-radius: 10px;
 }
 .content2 {
   cursor: pointer;
+}
+@media screen and (min-width: 250px) and (max-width: 380px) {
+  .box {
+  height:670px;
+  width: 380px;
+ 
+}}
+@media screen and (min-width: 381px) and (max-width: 550px) {
+  .box {
+  height:650px;
+  width: 420px;
+}
 }
 </style>
